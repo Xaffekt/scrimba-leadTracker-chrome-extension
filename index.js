7af584b9bd901db03a841762ed79868aed57f093
Xavier Minnamon <xaffekt.dev@gmail.com>
2023-06-09 16:42:04 -0700

    update

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
 
-let myLeads = ["aaa","apple"];
+let myLeads = [];
 
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click",function(){
-    console.log("Button clicked from addEventListener")
-
      myLeads.push(inputEl.value);
-     console.log(myLeads);
+     inputEl.value = "";
+     renderLeads();
 })
 
-let listItems = "";
-
-for(let i = 0; i < myLeads.length; i++)
+function renderLeads()
 {
-   listItems += "<li>"+ myLeads[i] +"</li>";
+    let listItems = "";
 
-    // create element
-    // set text content
-    // append to ul
-
-    /*
-        const li = document.createElement("li")
-        li.textContent = myLeads[i]
-        ulEl.append(li) 
-    */
+    for(let i = 0; i < myLeads.length; i++)
+    {
+        //clean way to use inner.html
+        //called template literals(string)
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>`;
+    } 
+    ulEl.innerHTML = listItems; 
+    //manipulating the dom once has better performance than constantly updating it
 }
 
-ulEl.innerHTML = listItems; 
-//manipulating the dom once has better performance than constantly updating it
+    /*Coding alternatives
+        create element
+            const li = document.createElement("li")
+        set text content
+            li.textContent = myLeads[i]
+        append to ul
+            ulEl.append(li) 
+            
+        Messy way to use inner.html
+        listItems += "<li><a target='_blank' href='"+myLeads[i]+"'>"+ myLeads[i] +"</a></li>";  
+    */
