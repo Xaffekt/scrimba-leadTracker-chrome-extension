e4baf85ae5ea5b23c41b338f5d005f09e0701b0d
Xavier Minnamon <xaffekt.dev@gmail.com>
2023-06-09 17:58:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+ 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+console.log(leadsFromLocalStorage)
+
 
 inputBtn.addEventListener("click",function(){
-     myLeads.push(inputEl.value);
-     inputEl.value = "";
-     renderLeads();
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
+    renderLeads();
 })
 
 function renderLeads()
